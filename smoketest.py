22bf7f48d120ad8c797849031248c32985276b75
Prasad459 <58033639+Prasad459@users.noreply.github.com>
2020-03-21 19:46:53 +0530

    Update smoketest.py

--- smoketest.py
+++ smoketest.py
@@ -18,7 +18,7 @@ driver = webdriver.Chrome()
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 # test deployment url
-url = "http://ec2-54-152-82-141.compute-1.amazonaws.com:8080/"
+url = "http://ec2-34-207-164-207.compute-1.amazonaws.com:8080/"
 driver.get(url)
 response = requests.get(url)
 try:

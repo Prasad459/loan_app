5664e853b1adbeb71ba5415a8e6ea5cb2326d1e7
Prasad459 <58033639+Prasad459@users.noreply.github.com>
2020-03-21 22:19:51 -0700

    Update smoketest.py

--- smoketest.py
+++ smoketest.py
@@ -14,7 +14,8 @@ from selenium.webdriver.common.keys import Keys
 import requests
 
 # initialize driver with browser
-driver = webdriver.Chrome()
+chromedriver = "C:\Users\Prasad\Downloads\chromedriver_win32"
+driver = webdriver.Chrome(chromedriver)
 #driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 #driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 # test deployment url
